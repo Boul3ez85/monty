ded631cfd037cb0f00fc780903f7a39baaf8fab8
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-14 00:31:38 +0100

    added err handling

--- get_instruction.c
+++ get_instruction.c
@@ -17,10 +17,8 @@ void get_instructions(char **tokens, stack_t **stack, unsigned int line_number)
 		{"pall", pall},
 		{NULL, NULL}
 	};
-
-	if (atoi(tokens[1]) != 0)
-		stack_value = atoi(tokens[1]);
-	if ((strcmp(tokens[0], "push") == 0) && isdigit(stack_value) != 0)
+	stack_value = atoi(tokens[1]);
+	if ((strcmp(tokens[0], "push") == 0) && isdigit(stack_value) == 0)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
