d1a4e69cd430c59b246dfceb94f7020fd3eb65ec
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-14 01:32:13 +0100

    handled pint error

--- op_functions.c
+++ op_functions.c
@@ -50,7 +50,10 @@ void pall(stack_t **stack, unsigned int line_number)
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+	if (stack_len(*stack) <= 0 || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	printf("%d\n", (*stack)->n);
 }
