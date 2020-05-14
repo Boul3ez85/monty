b7c1b3201883c9411c4e6e83424487090eca8ec3
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-14 04:07:30 +0100

    memory errors fixed

--- get_instruction.c
+++ get_instruction.c
@@ -9,7 +9,7 @@
  * Return: Void
  */
 
-void get_instructions(char **tokens, stack_t **stack, char* buffer, unsigned int line_number)
+void get_instructions(char **tokens, stack_t **stack, unsigned int line_number)
 {
 	int i = 0;
 	char *check = tokens[1];
@@ -26,7 +26,6 @@ void get_instructions(char **tokens, stack_t **stack, char* buffer, unsigned int
 		{
 			fprintf(stderr, "L%d: usage: push integer\n", line_number);
 			free(tokens);
-			free(buffer);
 			free_stack(*stack);
 			exit(EXIT_FAILURE);
 		}
--- monty.h
+++ monty.h
@@ -40,7 +40,7 @@ typedef struct instruction_s
 } instruction_t;
 
 /* Functions Prototypes */
-void get_instructions(char **tokens, stack_t **stack, char *buffer, unsigned int line_number);
+void get_instructions(char **tokens, stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 char **tokenizer(char *str);
